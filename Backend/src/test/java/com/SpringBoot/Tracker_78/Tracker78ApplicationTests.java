4802067d8447b0c97c357f54d0385555c0fd89a8
Seth Charles Agyei Mensah <sethcharlesagyeimensah@Seths-MacBook-Pro.local>
2025-07-28 11:27:45 +0000

    updated backend2

---
+++ Backend/src/test/java/com/SpringBoot/Tracker_78/Tracker78ApplicationTests.java
@@ -0,0 +1,15 @@
+package com.SpringBoot.Tracker_78;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+@SpringBootTest
+@ActiveProfiles("test")
+public class Tracker78ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
